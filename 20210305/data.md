fb38cdd5344bdabc97346c14502b78ce6d442c61
Kevoliv <56036350+Kevoliv@users.noreply.github.com>
2021-03-05 15:16:10 +0100

    corrections

---
+++ 20210305/data.md
@@ -0,0 +1,3 @@
+# Preparation des données
+
+J'ai filtré les données pour obtenir toutes les communes du canton de neuchâtel.
--- 20210305/prepare.js
+++ 20210305/prepare.js
@@ -6,7 +6,7 @@ const VilleEtGW = d => d.Canton === 'Neuchâtel'
 const resultat = data
   .filter(VilleEtGW)
   .map(d => ({ Commune: d.MunicipalityName, GWh: d.Scenario3_RoofsFacades_PotentialSolarElectricity_GWh }))
-  .sort((a, b) => a.nombre > b.nombre ? -1 : 1)
+  .sort((a, b) => a.GWh > b.GWh ? -1 : 1)
 
 console.log(resultat)
 console.log(JSON.stringify(result))
