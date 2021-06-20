6a10b89f38c43ab2b29bad977467c1f1c7b3effe
Kevoliv <56036350+Kevoliv@users.noreply.github.com>
2021-06-20 21:41:19 +0200

    Ajout de points sur la carte + dessin des circuits

--- Projet/projet/src/index.js
+++ Projet/projet/src/index.js
@@ -2,19 +2,47 @@ import L from 'leaflet'
 import arbres from './arbres.json'
 import batiments from './batiments.json'
 import circuit_trace from './circuit_trace.json'
+import test from './test.json'
 
-const map = L.map('map').setView([46.78104, 6.64707], 17)
+const map = L.map('map').setView([45.62218251898255, 9.284766257898681], 14)
 
 var myStyle = {
   "color": "red",
-  "weight": 5,
+  "weight": 8,
   "opacity": 0.65
 };
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+/**/var f1LiveData = $.ajax({
+  url:"http://ergast.com/api/f1/circuits.json?limit=100",
+  dataType: "json",
+  success: function(data) {
+    console.log(data);
+    for (var i=0 ; i<data.MRData.CircuitTable.Circuits.length ; i++) {
+      L.marker([data.MRData.CircuitTable.Circuits[i].Location.lat, data.MRData.CircuitTable.Circuits[i].Location.long], { icon }).addTo(map).bindPopup(data.MRData.CircuitTable.Circuits[i].circuitName);
+  }
+   },
+  error: function (xhr) {
+    alert(xhr.statusText)
+  }
+})
+
+
+
 L.tileLayer(
-  'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
+ // 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}',
+ //'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}',
+ 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.{ext}',
   {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     subdomains: 'abcd',
     minZoom: 0,
     maxZoom: 20,
@@ -28,13 +56,19 @@ const icon = L.icon({
   iconAnchor: [15, 30],
 })
 
+/*
 arbres.map(d => {
   const [lon, lat] = d
   L.marker([lat, lon], { icon }).addTo(map)
-})
+})*/
+
+
 
 L.geoJSON(
   circuit_trace,{
   style: myStyle
   }
 ).addTo(map)
+
+
+
